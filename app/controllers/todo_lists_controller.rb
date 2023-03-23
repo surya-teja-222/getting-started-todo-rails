91b290631ec97438c251e87c6bdc20fab0d2f994
Surya Teja Reddy <suryateja.dwarampudi@scaler.com>
2023-03-23 11:46:23 +0530

    Updated application styles

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -50,12 +50,12 @@ class TodoListsController < ApplicationController
   # DELETE /todo_lists/1 or /todo_lists/1.json
   def destroy
     @todo_list.destroy
-
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: "Todo list was successfully destroyed." }
+      format.html { redirect_to root_url, notice: 'Todo list was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
